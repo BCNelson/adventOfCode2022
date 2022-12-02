8991bbacb8e5f83afab1eecf2f79241c62de25c7
Bradley Nelson <bradley@nel.family>
2022-12-02 05:14:24 +0000

    setup

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $(basename "$0") <project> [input]"
+    exit 1
+fi
+
+# check if a second argument is given
+if [ $# -eq 2 ]; then
+    args=$(readlink -f "$2")
+    echo "Using input file: $args"
+else
+    args=""
+fi
+
+# change to the directory of this script
+pushd "$(dirname "$0")" > /dev/null || exit 2
+
+
+
+cd "$1" || (echo "Could not find $1" && exit 3)
+echo "Running the exacutable for $1"
+dotnet run "$args"
+
+popd > /dev/null || exit 4
