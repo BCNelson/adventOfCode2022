3dea9acbe43b793e1cf179d223835dda0cbacf56
Bradley Nelson <bradley@nel.family>
2022-12-03 06:34:06 +0000

    Day 1 part 2

--- day1/Program.cs
+++ day1/Program.cs
@@ -2,7 +2,7 @@
 class Day1 {
     static void Main(string[] args) {
         StreamReader file = new StreamReader(args[0]);
-        int Largest = 0;
+        List<int> counts = new List<int>();
         int CurrentTotal = 0;
         while (!file.EndOfStream) {
             string? line = file.ReadLine();
@@ -11,16 +11,17 @@ class Day1 {
                 continue;
             }
             if (line.Length == 0){
-                if (CurrentTotal > Largest)
-                    Largest = CurrentTotal;
+                counts.Add(CurrentTotal);
                 CurrentTotal = 0;
                 continue;
             }
             // do something with line
             CurrentTotal += int.Parse(line);
         }
-        if (CurrentTotal > Largest)
-            Largest = CurrentTotal;
-        Console.WriteLine(Largest);
+        counts.Prepend(CurrentTotal);
+        counts.Sort();
+        counts.Reverse();
+        Console.WriteLine("Part 1: " + counts[0]);
+        Console.WriteLine("Part 2: " + (counts[0] + counts[1] + counts[2]));
     }
 }
